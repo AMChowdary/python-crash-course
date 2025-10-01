6f64c0fde923e0099b9d66aad6ec225ac2a5a312
amchowdaryy <abdulmomin123999@gmail.com>
2025-10-01 20:06:40 +0530

    Organize

---
+++ .history/Chapter 02/2.1 Simple Message_20251001195944.py
@@ -0,0 +1,3 @@
+#Simple Message
+message = "Hello, world!"
+print(message)
---
+++ .history/Chapter 02/2.2 Simple Messages_20251001200009.py
@@ -0,0 +1,5 @@
+#Simple Messages Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message.
+message = "Hello, world!"
+print(message)
+message = "Hello, Python!"
+print(message)
