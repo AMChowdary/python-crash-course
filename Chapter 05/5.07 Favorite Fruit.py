45d12ce65c6f580bf43ac53b02f6476f553472d0
amchowdaryy <abdulmomin123999@gmail.com>
2025-10-22 23:26:16 +0530

    Started Chapter 05

---
+++ Chapter 05/5.07 Favorite Fruit.py
@@ -0,0 +1,16 @@
+# Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+# •	 Make a list of your three favorite fruits and call it favorite_fruits.
+# •	 Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!
+
+favorite_fruits = ['bananas', 'mangoes', 'strawberries']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'mangoes' in favorite_fruits:
+    print("You really like mangoes!")
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'oranges' in favorite_fruits:
+    print("You really like oranges!")
---
+++ Chapter 05/5.09 No Users.py
@@ -0,0 +1,25 @@
+# Add an if test to hello_admin.py to make sure the list of users is not empty.
+# •	 If the list is empty, print the message We need to find some users!
+# •	 Remove all of the usernames from your list, and make sure the correct message is printed.
+
+usernames = ['admin', 'Eric', 'Anna', 'Tom', 'Lisa']
+
+if usernames:
+    for user in usernames:
+        if user == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {user}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
+
+usernames.clear()
+
+if usernames:
+    for user in usernames:
+        if user == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {user}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
